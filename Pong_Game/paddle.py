98a6a9133573ef337dfde81588c4371363d4b512
Albert <changmiao.yu@mail.utoronto.ca>
2022-12-06 18:01:44 -0500

    Add files via upload

---
+++ Pong_Game/paddle.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, starting_position):
+        super().__init__()
+        # Create a paddle
+        self.color("white")
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(starting_position)
+
+        # Define up and down function
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
