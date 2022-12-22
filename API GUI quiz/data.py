6a31888eadd98592e1ee757013f1797da06a398f
Albert <changmiao.yu@mail.utoronto.ca>
2022-12-22 01:41:40 -0500

    Add files via upload

---
+++ API GUI quiz/data.py
@@ -0,0 +1,13 @@
+import requests
+NUMBER_OF_QUESTIONS = 10
+TYPE = "boolean"
+
+parameters = {
+    "amount": NUMBER_OF_QUESTIONS,
+    "type": TYPE
+}
+
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+question_data = response.json()["results"]
+
